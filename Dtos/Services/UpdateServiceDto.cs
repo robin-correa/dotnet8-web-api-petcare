c5df74b71a68b3950a1a2e02c3eec71f2f00efc9
robin.correa <robin.correa21@gmail.com>
2024-04-07 00:04:48 +0800

    Implement model validations to request DTOs for services API.

--- Dtos/Services/UpdateServiceDto.cs
+++ Dtos/Services/UpdateServiceDto.cs
@@ -1,11 +1,20 @@
-﻿namespace dotnet8_web_api_petcare.Dtos.Services
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnet8_web_api_petcare.Dtos.Services
 {
     public class UpdateServiceDto
     {
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
         public string? Description { get; set; }
-        public int MinPrice { get; set; }
-        public int MaxPrice { get; set; }
-        public int Status { get; set; }
+
+        [Required(ErrorMessage = "MinPrice must be a required")]
+        public int? MinPrice { get; set; }
+
+        [Required(ErrorMessage = "MaxPrice must be a required")]
+        public int? MaxPrice { get; set; }
+
+        [Required(ErrorMessage = "Status must be a required")]
+        public int? Status { get; set; }
     }
 }
