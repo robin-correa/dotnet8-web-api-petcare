a7b3011283940ce2e451f5ed2f8fc19a809f7f85
robin.correa <robin.correa21@gmail.com>
2024-04-06 10:39:17 +0800

    Install EFC packages and add DB connection string, context and services domain model.

---
+++ Models/Domains/Service.cs
@@ -0,0 +1,14 @@
+﻿namespace dotnet8_web_api_petcare.Models.Domains
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal MinPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+        public int Status { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
