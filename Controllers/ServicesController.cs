e93c7e6ed914a34cff847e4634119fa911bffb23
robin.correa <robin.correa21@gmail.com>
2024-04-06 19:17:43 +0800

    Add create, update, delete API functionalities.

--- Controllers/ServicesController.cs
+++ Controllers/ServicesController.cs
@@ -1,5 +1,6 @@
 ﻿using dotnet8_web_api_petcare.Data;
 using dotnet8_web_api_petcare.Dtos.Services;
+using dotnet8_web_api_petcare.Models.Domains;
 
 using Microsoft.AspNetCore.Mvc;
 
@@ -48,6 +49,99 @@ namespace dotnet8_web_api_petcare.Controllers
             {
                 return NotFound();
             }
+
+            var serviceResource = new GetServiceDto
+            {
+                Id = service.Id,
+                Name = service.Name,
+                Description = service.Description,
+                MinPrice = service.MinPrice,
+                MaxPrice = service.MaxPrice,
+                Status = service.Status,
+                CreatedAt = service.CreatedAt,
+                UpdatedAt = service.UpdatedAt,
+            };
+
+            return Ok(serviceResource);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateServiceDto request)
+        {
+            var service = new Service
+            {
+                Name = request.Name,
+                Description = request.Description,
+                MinPrice = request.MinPrice,
+                MaxPrice = request.MaxPrice,
+                Status = request.Status,
+            };
+
+            _appDbContext.Services.Add(service);
+            _appDbContext.SaveChanges();
+
+            var serviceResource = new GetServiceDto
+            {
+                Id = service.Id,
+                Name = service.Name,
+                Description = service.Description,
+                MinPrice = service.MinPrice,
+                MaxPrice = service.MaxPrice,
+                Status = service.Status,
+                CreatedAt = service.CreatedAt,
+                UpdatedAt = service.UpdatedAt,
+            };
+
+            return CreatedAtAction(nameof(Show), new { id = service.Id }, serviceResource);
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateServiceDto request)
+        {
+            var service = _appDbContext.Services.FirstOrDefault(x => x.Id == id);
+
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            service.Name = request.Name;
+            service.Description = request.Description;
+            service.MinPrice = request.MinPrice;
+            service.MaxPrice = request.MaxPrice;
+            service.Status = request.Status;
+
+            _appDbContext.SaveChanges();
+
+            var serviceResource = new GetServiceDto
+            {
+                Id = service.Id,
+                Name = service.Name,
+                Description = service.Description,
+                MinPrice = service.MinPrice,
+                MaxPrice = service.MaxPrice,
+                Status = service.Status,
+                CreatedAt = service.CreatedAt,
+                UpdatedAt = service.UpdatedAt,
+            };
+
+            return Ok(serviceResource);
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var service = _appDbContext.Services.FirstOrDefault(x => x.Id == id);
+
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            _appDbContext.Services.Remove(service);
+            _appDbContext.SaveChanges();
 
             var serviceResource = new GetServiceDto
             {
---
+++ Dtos/Services/UpdateServiceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace dotnet8_web_api_petcare.Dtos.Services
+{
+    public class UpdateServiceDto
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int MinPrice { get; set; }
+        public int MaxPrice { get; set; }
+        public int Status { get; set; }
+    }
+}
