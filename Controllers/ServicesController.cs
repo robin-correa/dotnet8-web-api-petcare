9b7b6ca8f3c6539475a36e2d301a9659b99a452a
robin.correa <robin.correa21@gmail.com>
2024-04-06 15:04:14 +0800

    Add all and single records for services.

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,38 @@
+﻿using dotnet8_web_api_petcare.Data;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet8_web_api_petcare.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public ServicesController(AppDbContext dbContext)
+        {
+            _appDbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var services = _appDbContext.Services.ToList();
+            return Ok(services);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult Show([FromRoute] int id)
+        {
+            var service = _appDbContext.Services.FirstOrDefault(x => x.Id == id);
+
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(service);
+        }
+    }
+}
---
+++ Dtos/Services/GetServiceDto.cs
@@ -0,0 +1,14 @@
+﻿namespace dotnet8_web_api_petcare.Dtos.Services
+{
+    public class GetServiceDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int MinPrice { get; set; }
+        public int MaxPrice { get; set; }
+        public int Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
